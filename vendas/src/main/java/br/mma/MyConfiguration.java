02da996d3910688170eaa56ff2197dfada24605d
Marcos <mmatheusandrade2013@gmail.com>
2021-08-29 14:02:27 -0300

    Configurada conexão com banco H2

--- vendas/src/main/java/br/mma/MyConfiguration.java
+++ vendas/src/main/java/br/mma/MyConfiguration.java
@@ -2,8 +2,6 @@ package br.mma;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Development
 public class MyConfiguration {
